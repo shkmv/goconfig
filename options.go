20b92aed1972ee64820c6e916cf483feba2db053
A.Shakhmatov <dev@shakhmatov.com>
2025-04-26 11:34:31 +0300

    feat: add file source option

--- options.go
+++ options.go
@@ -2,10 +2,19 @@ package goconfig
 
 import "github.com/shkmv/goconfig/sources"
 
+// Option represents a configuration option.
 type Option func(*Config)
 
+// WithEnv adds an environment variable source to the configuration.
 func WithEnv(prefix string) Option {
 	return func(c *Config) {
 		c.sources = append(c.sources, sources.NewEnvSource(prefix))
+	}
+}
+
+// WithFile adds a file source to the configuration.
+func WithFile(path string) Option {
+	return func(c *Config) {
+		c.sources = append(c.sources, sources.NewFileSource(path))
 	}
 }
